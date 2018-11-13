f96eb7540d7de9d24c5ce6c1c606901927bb9bb1
sjoerd20 <sjoerd.terpstra@outlook.com>
2018-11-13 12:34:06 +0100

    added FriendsAdapter

--- app/src/main/java/com/example/sjoerd/friendsr/FriendsAdapter.java
+++ app/src/main/java/com/example/sjoerd/friendsr/FriendsAdapter.java
@@ -7,11 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.graphics.drawable.Drawable;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FriendsAdapter extends ArrayAdapter<Friend> {
+
+    private ArrayList<Friend> friends;
 
     @NonNull
     @Override
@@ -20,11 +25,25 @@ public class FriendsAdapter extends ArrayAdapter<Friend> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent,
                                              false);
         }
+
+        // get current friend
+        Friend friend = friends.get(position);
+
+        // draw image
+        ImageView image = convertView.findViewById(R.id.imageView);
+        Drawable imageDrawable = getContext().getResources().getDrawable(friend.getDrawableId());
+        image.setImageDrawable(imageDrawable);
+
+        // set name
+        TextView name = convertView.findViewById(R.id.textView);
+        name.setText(friend.getName());
+
         return convertView;
     }
 
     // constructor
     public FriendsAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Friend> objects) {
         super(context, resource, objects);
+        friends = objects;
     }
 }
--- app/src/main/java/com/example/sjoerd/friendsr/MainActivity.java
+++ app/src/main/java/com/example/sjoerd/friendsr/MainActivity.java
@@ -2,21 +2,34 @@ package com.example.sjoerd.friendsr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     // ArrayList of sample friends
-    private ArrayList<Friend> friends = new ArrayList<>();
+    private ArrayList<Friend> arrayListFriends = new ArrayList<>();
+    private String listFriends[] = {"arya", "cersei", "daenerys", "jaime", "jon", "jorah",
+                                    "margaery", "melisandre", "sansa", "tyrion"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO add friends to ArrayList friends
+        for (int i = 0; i < listFriends.length; i++) {
+            int resourceID = getResources().getIdentifier(listFriends[i], "drawable",
+                    getPackageName());
+            Friend friend = new Friend(listFriends[i], "This is " + listFriends[i], resourceID);
+            arrayListFriends.add(friend);
+        }
+
     }
 
+    // instantiate FriendsAdapter
+    FriendsAdapter adapter = new FriendsAdapter(this, R.layout.grid_item, arrayListFriends);
 
+    // show on grid
+    GridView gv = findViewById(R.id.gv);
 }
